07e70e179555ae2b945bfff330f840d8fc29f3a3
razShort <basti.gruno@gmx.de>
2021-10-18 15:40:54 +0200

    Added template and changed the design slightly

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 /*jslint browser:true */
-
+/* --------- VARIABLES ------------- */
 var darkmodeButton = document.querySelector(".darkmode");
 var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 var currentTheme = localStorage.getItem("theme");
 var userChoiseColorMode = prefersDarkScheme.matches ? "dark" : "light";
 
+var darkmodeSymbol = '<i class="uil uil-moon"></i>';
+var lightmodeSymbol = '<i class="uil uil-sun"></i>';
+
+
+
+/* --------- METHODS ------------- */
 // first visite on website
 if (currentTheme === null) {
   if (prefersDarkScheme.matches) {
@@ -13,17 +19,25 @@ if (currentTheme === null) {
   }
 }
 
+var darkmodeHTMLSymbol = lightmodeSymbol;
 // activates dark theme if user activated dark mode once
 if (currentTheme === "dark") {
   document.body.classList.toggle("dark-theme");
+  darkmodeHTMLSymbol = darkmodeSymbol;
 }
+darkmodeButton.innerHTML = darkmodeHTMLSymbol;
 
+// darkmode Click-listener
 darkmodeButton.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
 
   var theme = "light";
+  var darkmodeHTMLSymbol = lightmodeSymbol;
   if (document.body.classList.contains("dark-theme")) {
     theme = "dark";
+    darkmodeHTMLSymbol = darkmodeSymbol;
   }
+  // changing button symbol
+  darkmodeButton.innerHTML = darkmodeHTMLSymbol;
   localStorage.setItem("theme", theme);
 });
