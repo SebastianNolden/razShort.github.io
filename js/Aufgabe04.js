e0ce379e4d726376b8eef98d475033f4deb96e98
razShort <basti.gruno@gmx.de>
2021-10-28 14:42:52 +0200

    Rednerliste hinzugefügt

--- js/Aufgabe04.js
+++ js/Aufgabe04.js
@@ -1,9 +1,5 @@
 /* --------- VARIABLES ------------- */
-var inputField = document.getElementById("ItemInput");
-var addButton = document.getElementById("addItem");
-var deleteAll = document.getElementById("deleteAll");
-var list = document.getElementById("itemList");
-var einkaufsliste = document.querySelector(".Einkaufsliste");
+var home = document.querySelector(".home");
 
 /* Unteraufgaben & zugehörige Buttons */
 var firstExercise = document.querySelector(".Einkaufsliste");
@@ -14,19 +10,48 @@ var button02 = document.getElementById("button02");
 
 /* --------- METHODS ------------- */
 /* ------ Unteraufgaben wechseln ------ */
+setNormalBackgroundForMenuButtons();
+setActiveButtonBackground(button01);
+
 button01.addEventListener("click", function () {
   firstExercise.style.display = "block";
   secondExercise.style.display = "none";
+  setNormalBackgroundForMenuButtons();
+  setActiveButtonBackground(button01);
 });
 
 button02.addEventListener("click", function () {
   firstExercise.style.display = "none";
   secondExercise.style.display = "block";
+  setNormalBackgroundForMenuButtons();
+  setActiveButtonBackground(button02);
 });
+
+
+function setNormalBackgroundForMenuButtons(){
+  var backgroundColor = window.getComputedStyle(home).getPropertyValue('background-color');
+  button01.style.background = backgroundColor;
+  button02.style.background = backgroundColor;
+}
+
+function setActiveButtonBackground(button){
+  var body = document.getElementsByTagName("body")[0];
+  var color = window.getComputedStyle(body).getPropertyValue('background-color');
+  button.style.background = color;
+}
 
 
 
 /* ------ Einkaufsliste ------ */
+/* --------- VARIABLES ------------- */
+var inputField = document.getElementById("ItemInput");
+var addButton = document.getElementById("addItem");
+var deleteAll = document.getElementById("deleteAll");
+var listEinkauf = document.getElementById("itemList");
+var einkaufsliste = document.querySelector(".Einkaufsliste");
+
+
+/* --------- METHODS ------------- */
 /* lade den css style für einkaufsliste IE unterstützt getComputedStyle nicht.*/
 var einkaufsListeStyle = window.getComputedStyle ?
   window.getComputedStyle(einkaufsliste, null).getPropertyValue('display') : null;
@@ -35,17 +60,19 @@ if (einkaufsListeStyle !== "none") {
 }
 
 addButton.addEventListener("click", function(){
-  if (inputField.value.length === 0) {
-    return;
-  }
+  handleInput(inputField, createAndAddListItem);
   saveIteminLocalStorage(inputField.value);
-  createAndAddListItem(inputField.value);
+});
 
-  inputField.value = "";
+inputField.addEventListener("keyup", ({key}) => {
+  if (key === "Enter") {
+    handleInput(inputField, createAndAddListItem);
+    saveIteminLocalStorage(inputField.value);
+  }
 });
 
 deleteAll.addEventListener("click", function(){
-  list.innerHTML = "";
+  listEinkauf.innerHTML = "";
   localStorage.setItem('list', "");
 });
 
@@ -107,8 +134,207 @@ function createAndAddListItem(item){
     li.remove();
   });
 
-  list.append(li);
+  listEinkauf.append(li);
 }
 
 
 /* ------ Rednerliste ------ */
+/* --------- VARIABLES ------------- */
+var inputFieldRedner = document.getElementById("ItemInputRednerliste");
+var addButtonRedner = document.getElementById("addItemRednerliste");
+var deleteAllRedner = document.getElementById("deleteAllRednerliste");
+var listRedner = document.getElementById("itemListRednerliste");
+var rednerliste = document.querySelector(".Rednerliste");
+var resetAllRedner = document.getElementById("resetAllRednerliste");
+var sub = new alertAll();
+var id = 0;
+
+
+/* --------- METHODS ------------- */
+addButtonRedner.addEventListener("click", function(){
+  handleInput(inputFieldRedner, createAndAddRednerListItem);
+});
+
+inputFieldRedner.addEventListener("keyup", ({key}) => {
+  if (key === "Enter") {
+    handleInput(inputFieldRedner, createAndAddRednerListItem);
+  }
+});
+
+resetAllRedner.addEventListener("click", () => {
+  sub.resetAll();
+});
+
+deleteAllRedner.addEventListener("click", function(){
+  listRedner.innerHTML = "";
+  sub.removeAll();
+});
+
+function handleInput(input, func) {
+  if (input.value.length === 0) {
+    return;
+  }
+  func(input.value);
+  input.value = "";
+}
+
+function createAndAddRednerListItem(item){
+  var li = document.createElement("li");
+
+  var itemWrapper = document.createElement("div");
+  itemWrapper.classList.add("ItemWrapper");
+
+  var itemText = document.createElement("span");
+  itemText.classList.add("listText");
+  itemText.textContent = item;
+
+  var itemTime = document.createElement("span");
+  itemTime.classList.add("ZeitDarstellung");
+  itemTime.classList.add("listText");
+  itemTime.textContent = "00:00:00";
+
+  var itemStartTime = document.createElement("span");
+  itemStartTime.classList.add("button");
+  itemStartTime.textContent = "Start!";
+
+  var itemTimeReset = document.createElement("span");
+  itemTimeReset.classList.add("button");
+  itemTimeReset.textContent = "Reset";
+
+  var itemButton = document.createElement("span");
+  itemButton.classList.add("button");
+  itemButton.textContent = "Delete";
+
+  itemWrapper.append(itemText);
+  itemWrapper.append(itemTime);
+  itemWrapper.append(itemStartTime);
+  itemWrapper.append(itemTimeReset);
+  itemWrapper.append(itemButton);
+  li.append(itemWrapper);
+
+  var timer = new stopwatch(itemTime, itemStartTime, id++);
+  sub.add(timer);
+
+  itemButton.addEventListener("click", function(){
+    timer.stop();
+    sub.remove(timer);
+    li.remove();
+  });
+
+  itemStartTime.addEventListener("click", function(){
+    if (itemStartTime.textContent === "Start!") {
+      sub.alert(timer.id);
+      timer.start();
+    } else {
+      timer.stop();
+    }
+  });
+
+  itemTimeReset.addEventListener("click", function(){
+    timer.reset();
+  });
+
+  listRedner.append(li);
+}
+
+function alertAll(){
+  var subscriber = [];
+
+  function add(timer){
+    subscriber.push(timer);
+  }
+
+  function remove(timer){
+    var removeIndex = subscriber.findIndex(item => item.id === timer.id);
+    subscriber.splice(removeIndex, 1);
+    console.log(subscriber);
+  }
+
+  function alert(id){
+    for (var i = 0; i < subscriber.length; i++) {
+      if (subscriber[i].id !== id) {
+        subscriber[i].stop();
+      }
+    }
+  }
+
+  function resetAll(){
+    for (var i = 0; i < subscriber.length; i++) {
+      subscriber[i].stop();
+      subscriber[i].reset();
+    }
+  }
+
+  function removeAll(){
+    subscriber = [];
+  }
+
+  this.add = add;
+  this.remove = remove;
+  this.removeAll = removeAll;
+  this.resetAll = resetAll;
+  this.alert = alert;
+}
+
+function stopwatch(elem, button, id, delay){
+  /* init */
+  delay = delay || 100;
+  id = id;
+  reset();
+  var interval, offset, timer;
+
+  function start(){
+    if (!interval) {
+      button.textContent = "Stop!";
+      offset = Date.now();
+      interval = setInterval(update, delay);
+    }
+  }
+
+  function stop(){
+    if (interval) {
+      button.textContent = "Start!";
+      clearInterval(interval);
+      interval = null;
+    }
+  }
+
+  function reset(){
+    timer = 0;
+    button.textContent = "Start!";
+    renderTimer();
+  }
+
+  function update(){
+    var now = Date.now();
+    timer += (now - offset);
+    offset = now;
+    renderTimer();
+  }
+
+  function renderTimer(){
+    var timeRendered = "00:00:00";
+    var date = new Date(timer);
+    var hours = date.getHours() - 1;
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    timeRendered = `${hours}:${minutes}:${seconds}`
+    elem.textContent = timeRendered;
+  }
+
+  this.start = start;
+  this.stop = stop;
+  this.reset = reset;
+  this.id = id;
+}
