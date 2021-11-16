89070327b627d6663bc7a17115a246964817d893
razShort <basti.gruno@gmx.de>
2021-11-16 17:16:21 +0100

    Added Exercise 6 to the webpage

--- js/Aufgabe06.js
+++ js/Aufgabe06.js
@@ -3,23 +3,18 @@ const home = document.querySelector(".home");
 const button01 = document.getElementById("button01");
 const button02 = document.getElementById("button02");
 const button03 = document.getElementById("button03");
-const button04 = document.getElementById("button04");
-const button05 = document.getElementById("button05");
 const exerciseArray = [
-  "Aufgabe02/flexbox_Desktop_first.html",
-  "Aufgabe02/grid_Mobile_first.html",
-  "Aufgabe02/Holy_grail_FLexbox.html",
-  "Aufgabe02/Holy_grail_Grid.html",
-  "Aufgabe02/Landing_Page_Grid.html"
+  "Aufgabe06/svg_balken.html",
+  "Aufgabe06/svg_bezier.html",
+  "Aufgabe06/svg_Kalli.html",
 ];
-const buttons = [button01, button02, button03, button04, button05];
-
+const buttons = [button01, button02, button03];
 
 /* --------- METHODS ------------- */
 /* ------ Unteraufgaben wechseln ------ */
 setNormalBackgroundForMenuButtons();
 setActiveButtonBackground(0);
-clickAufgabenButton(0)
+clickAufgabenButton(2)
 
 for (var i = 0; i < buttons.length; i++) {
   addEventClick(buttons[i], i);
